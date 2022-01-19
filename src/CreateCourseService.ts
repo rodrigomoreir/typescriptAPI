af781fb9f63bf8d9e954ba2e7d58bdd64d89906f
Rodrigo Moreira <rodrigoalvesm06@gmail.com>
2022-01-19 20:47:35 -0300

    creates a node project with typescript

---
+++ src/CreateCourseService.ts
@@ -0,0 +1,9 @@
+
+class CreateCourseService {
+
+    execute(name: string, duration: number, educator: string) {
+        console.log(name, duration, educator)
+    }
+}
+
+export default CreateCourseService
