8eee5915944351463e6c1923b740eba3d7916319
Rodrigo Moreira <rodrigoalvesm06@gmail.com>
2022-01-24 09:14:42 -0300

    chore: creates repository

--- src/categories.routes.ts
+++ src/categories.routes.ts
@@ -1,24 +1,28 @@
-import { Router } from 'express'
+import { response, Router } from 'express'
 import { Category } from './model/Category'
+import { CategoriesRepository } from './repositories/CategoriesRepository'
 
 const categoriesRoutes = Router()
-
-const categories: Category[] = []
+const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body
 
-  const category: Category = new Category()
+  const categoryAlreadyExists = categoriesRepository.findByName(name)
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date()
-  })
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: 'Category already exists!' })
+  }
 
-  categories.push(category)
+  categoriesRepository.create({ name, description })
 
-  return response.status(201).json(categories)
+  return response.status(201).send()
+})
+
+categoriesRoutes.get('/', (request, response) => {
+  const allCategories = categoriesRepository.list()
+
+  return response.json(allCategories)
 })
 
 export { categoriesRoutes }
