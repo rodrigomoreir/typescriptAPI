998019fcd91d044bd41aaf31cb1d22a3a7ff698e
Rodrigo Moreira <rodrigoalvesm06@gmail.com>
2022-02-03 20:02:34 -0300

    chore: SRP and DIP (SOLID)

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,26 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository"
+
+interface IRequest {
+  name: string;
+  description: string
+}
+
+// Service é camada de Alto Nível, que está mais proximo ao Dominio
+// Rotas é camada de Baixo Nível, mais próximo ao usuário
+class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) { }
+
+  execute({ name, description }: IRequest): void {
+    // Para acessar uma variável de dentro do Constructor, precisa usar o 'this.'
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name)
+
+    if (categoryAlreadyExists) {
+      // Forma de retorar o erro no service
+      throw new Error("Category already exists!")
+    }
+
+    this.categoriesRepository.create({ name, description })
+  }
+}
+
+export { CreateCategoryService }
