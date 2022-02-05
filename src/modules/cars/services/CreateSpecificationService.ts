d6d37bb1b6a7f5a303de1a195964d95c8197a4c1
Rodrigo Moreira <rodrigoalvesm06@gmail.com>
2022-02-05 08:55:39 -0300

    chore: create routes and specifications methods

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,9 +1,24 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository"
 
+interface IRequest {
+  name: string
+  description: string
+}
 
 class CreateSpecificationService {
+  constructor(private specificationsRepository: ISpecificationsRepository) {
 
-  execute() {
-    console.log('SpecificationService')
+  }
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists = this.specificationsRepository.findByName(name)
+
+    if (specificationAlreadyExists) {
+      // Forma de retorar o erro no service
+      throw new Error("Specification already exists!")
+    }
+
+    this.specificationsRepository.create({ name, description })
   }
 
 }
